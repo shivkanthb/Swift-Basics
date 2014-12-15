049912682f0dd405345787ae80c08e0569dac10b
shivkanth <shivkanthb@gmail.com>
2014-12-14 17:31:26 -0700

    types

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,32 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var str1: String = "this is an explicit type"
+
+print(str + "\n")
+
+var v1 = "test1"
+var v2 = "test"
+var v3 = "\(v1) \(v2) test3"
+// string interpolation
+
+let c1 = "sun rises in the east"
+// this is a constant. 'let' is for declaring constants
+
+
+
+
+//lets look at numbers
+
+let n1 = 2014 // type Int or Integer
+
+var version = 1.0 // type Double
+
+// if you want float, then needs to be explicitly mentioned like version: Float = 1.0
+
+print(n1) // i get an error if I type " " + n1 inside.
+
+var isAwesome = true // type : Bool
